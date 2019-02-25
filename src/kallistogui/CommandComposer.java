7b11c12614e36304bd62dc6fb2e8315495c825b2
Halforc Null <yaorunan@gmail.com>
2019-02-24 20:24:03 -0600

    adding command composer ( not implemented )

---
+++ src/kallistogui/CommandComposer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kallistogui;
+
+/**
+ *
+ * @author Halforc
+ */
+public class CommandComposer {
+    String ComposeIndexCommand(){
+        return "Not Impelement";
+    }
+    
+    String ComposeQuantCommand(){
+        return "Not Impelement";
+    }
+}
--- src/kallistogui/MainForm.java
+++ src/kallistogui/MainForm.java
@@ -5,12 +5,6 @@ import javax.swing.DefaultListModel;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author runan.yao
